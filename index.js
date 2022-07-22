1d4ac288af39f5c5ee565964e6a1f0a5b847eaaa
Noa/Nocly/Enzo <enzo.bedos.13@gmail.com>
2022-07-23 00:17:27 +0200

    new file:   commands/ping.js 	new file:   commands/server.js 	modified:   index.js 	modified:   package.json 	modified:   register.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // Variable
-const { Client, GatewayIntentBits} = require('discord.js');
+const { Client, GatewayIntentBits, Collection} = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
-const { token} = require("./data/config.json")
+const { token} = require("./data/config.json");
+const fs = require('node:fs');
+const path = require('node:path');
+
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandsFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+
+	client.commands.set(command.data.name, command);
+}
 
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag} !`);
@@ -10,11 +23,25 @@ client.on('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isChatInputCommand()) return;
 
-	if(interaction.commandName === "ping") {
-		await interaction.reply('Pong');
+	const command = client.commands.get(interaction.commandName);
+	
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (err) {
+		console.log(err);
+		await interaction.reply({content:'There was an error while executing this command!', ephemeral: true });
 	}
+
+	//const {commandName} = interaction;
+
+	//if(commandName === "ping") {
+	//	await interaction.reply('Pong');
+	//} else if (commandName === 'server') {
+	//	await interaction.reply(`Server name: ${interaction.guild.name} \nTotal members: ${interaction.guild.memberCount}`)
+	//}
 });
 
 
-//test sync with git
 client.login(token);
--- register.js
+++ register.js
@@ -1,13 +1,28 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord.js');
-const { token, clientID, guildID } = require('./data/config.json')
+const { Routes, SlashCommandBuilder } = require('discord.js');
+const { token, clientID, guildID, guildJarvis } = require('./data/config.json')
 
 const commands = [
-	{
-		name: 'ping',
-		description: 'Replies with pooooooong!',
-	},
-];
+	//{
+	//	name: 'ping',
+	//	description: 'Replies with pooooooong!',
+	//},
+
+	//new SlashCommandBuilder().setName('ping').setDescription('Replies with Ponnnng!'),
+	//new SlashCommandBuilder().setName('server').setDescription('Replies with server data'),
+	//new SlashCommandBuilder().setName('user').setDescription('Replies with user data')
+]//.map(command => command.toJSON());
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (file of commandsFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON())
+}
 
 const rest = new REST({ version: '10'}).setToken(token);
 
